84ae55d6ffd7756dbb488ff827163405eea6adb7
sahed009 <saheduzzamansobuj@gmail.com>
2025-04-09 21:53:29 +0600

    project initiated

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+      <h1 className="text-7xl font-bold text-blue-600">Hello, Vite + Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
